844b6a6f918e566cd1514300b218f62a84d72a56
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-05-20 14:37:10 +0200

    inverse

--- AmPm.py
+++ AmPm.py
@@ -1,6 +1,8 @@
 #%%
 import matplotlib.pyplot as plt
 
+
+#%%
 am = [1,2,3,4,5,6,7,8,9,10,11,0]
 pm = [13,14,15,16,17,18,19,20,21,22,23,12]
 ampm = [[am[i],pm[i]] for i in range(len(am))]
@@ -24,6 +26,25 @@ plt.legend()
 plt.savefig('ploooot.png')
 # %%
 
+hours_passed = [i+1 for i in range(24)]
+
+am = [1,2,3,4,5,6,7,8,9,10,11,None,None,None,None,None,None,None,None,None,None,None,None,12]
+pm = [None,None,None,None,None,None,None,None,None,None,None,12,1,2,3,4,5,6,7,8,9,10,11,None]
+
+plt.xlabel('hours passed since new day')
+plt.ylabel('number shown')
+plt.xticks(hours_passed[1::2])
+plt.yticks([i+1 for i in range(12)])
+plt.grid(c='#dddddd')
+
+plt.scatter(hours_passed,am,marker='o',label='am',zorder=2)
+plt.scatter(hours_passed,pm,marker='o',label='pm',zorder=2)
+
+plt.title('Why AM and PM is confusing (inversed)',size=14, pad=24)
+#plt.plot(count,am,marker='o',label='am')
+#plt.plot(count,pm,marker='o',label='pm')
+plt.legend()
+plt.savefig('plooootInverse.png')
 
 
 # %%
